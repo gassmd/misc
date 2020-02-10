9062905ccb4463aa896b95d17da72916d2e46a09
gassmd <51835798+gassmd@users.noreply.github.com>
2020-02-10 16:54:56 -0500

    Add files via upload

--- calculator/index.js
+++ calculator/index.js
@@ -5,58 +5,71 @@
 
 */
 
+var num1, op, num2, ans;
+var eqnString;
+var testEquation = new Array();
+
 function display(val) {
   document.getElementById("result").value += val;
   console.log("displayed number " + val);
 }
 
 function clearDis() {
-  console.log("cleared display");
   document.getElementById("result").value = " ";
+  console.log("cleared display");
 }
 
-function storeNum1() {
-  var num1 = document.getElementById("result").value;
+function storeNum() {
+  num1 = document.getElementById("result").value;
   console.log("stored " + num1 + " as 'num1'");
-  clearDis();
   return num1;
 }
 
 function storeNum2() {
-  var num2 = document.getElementById("result").value;
+  num2 = document.getElementById("result").value;
   console.log("2nd stored " + num2);
   return num2;
 }
 
-function operation(op) {
-  const num1 = storeNum1();
-  if (num1) {
-    const num2 = storeNum2();
-  }
-  var ans;
-  switch (op) {
-    case "+":
-      ans = num1 + num2;
-      console.log(op + " stored, " + num1 + " stored");
-      break;
-    case "-":
-      ans = num1 - num2;
-      console.log(op + " stored, + " + num1 + " stored");
-      break;
-    case "*":
-      ans = num1 * num2;
-      console.log(op + " stored, + " + num1 + " stored");
-      break;
-    case "/":
-      ans = num1 / num2;
-      console.log(op + " stored, + " + num1 + " stored");
-      break;
-  }
+function storeOperation(op) {
+    storeNum(); 
+    console.log("stored operation: " + op);
+    console.log(num1 + "    " + op)
+    clearDis();
+    testEquation.push(num1);
+    testEquation.push(op);
+    console.log(testEquation);
+}
 
-  return ans;
+
+  
+
+
+function displayEquation(){
+  console.log(testEquation);
+  for (i = 0; i < testEquation.length; i++)
+   document.getElementById("equation").innerHtml = ((i+1) + ": " + testEquation[i]);
+
 }
 
 function equals() {
-  document.getElementById("result").value = operation("-");
-  console.log(operation("-"));
+  storeNum();
+  testEquation.push(num1);
+  displayEquation();
+  testEquals();
+}
+
+function testEquals(){
+  var testOp, testNum1, testNum2, testAns;
+  for (i = 0; i < testEquation.length; i++){
+    if (testEquation[i] == "*" || testEquation[i] == "/"){
+      testOp = testEquation[i];
+      console.log(testOp + " at position " + i);
+      testNum1 = testEquation[i-1];
+      testNum2 = testEquation[i+1];
+      console.log("stored numbers: " + testNum1 + "," + testNum2);
+      testAns = eval(testNum1+testOp+testNum2); 
+      console.log(testAns); 
+    }    
+  }
 }
